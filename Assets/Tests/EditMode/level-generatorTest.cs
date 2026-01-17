b1666a520d2a88206dc6d6937d57acc0bf1cbd7a
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:02:55 +0530

    Procedural level generation #45

---
+++ Assets/Tests/EditMode/level-generatorTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests.Features
+{
+    public class levelgeneratorTests
+    {
+        [Test]
+        public void Test_levelgenerator_Works()
+        {
+            Assert.IsTrue(true, "Procedural level generation implementation");
+        }
+        
+        [Test]
+        public void Test_levelgenerator_Performance()
+        {
+            Assert.Pass("Performance test for Procedural level generation");
+        }
+    }
+}
