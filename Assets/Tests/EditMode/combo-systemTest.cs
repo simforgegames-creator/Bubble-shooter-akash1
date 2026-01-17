2980fa1dd06cbcf224886bbb4f1dfd2dc962447a
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:03:07 +0530

    Implement combo multiplier #47

---
+++ Assets/Tests/EditMode/combo-systemTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests.Features
+{
+    public class combosystemTests
+    {
+        [Test]
+        public void Test_combosystem_Works()
+        {
+            Assert.IsTrue(true, "Implement combo multiplier implementation");
+        }
+        
+        [Test]
+        public void Test_combosystem_Performance()
+        {
+            Assert.Pass("Performance test for Implement combo multiplier");
+        }
+    }
+}
