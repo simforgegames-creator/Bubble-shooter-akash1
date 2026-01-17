bfe797f37509b3884657c564290793b748a240a4
SimForge Games <simforgegames@gmail.com>
2026-01-17 13:07:00 +0530

    Feat: Feature_1768635417_12 implementation (Closes #124)

---
+++ Assets/Tests/PlayMode/Feature_1768635417_12PlayTests.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using BubbleShooter.Features.Feature_1768635417_12;
+namespace Tests {
+    public class Feature_1768635417_12PlayTests {
+        [UnityTest]
+        public IEnumerator Controller_Runs_Test() {
+            var go = new GameObject();
+            var ctrl = go.AddComponent<Feature_1768635417_12Controller>();
+            var view = go.AddComponent<Feature_1768635417_12View>();
+            ctrl.Initialize();
+            ctrl.Execute();
+            yield return null;
+            Assert.IsTrue(ctrl.GetMetric() > 0);
+        }
+    }
+}
