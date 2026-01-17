488104fe51458b31ed58908d519915ae2aad93a1
SimForge Games <simforgegames@gmail.com>
2026-01-17 13:15:54 +0530

    Feat: Feature_1768635951_37 implementation (Closes #204)

---
+++ Assets/Scripts/Features/Feature_1768635951_37/IFeature_1768635951_37Service.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+namespace BubbleShooter.Features.Feature_1768635951_37 {
+    public interface IFeature_1768635951_37Service {
+        void Initialize();
+        void Execute();
+        float GetMetric();
+    }
+}
