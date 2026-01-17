f8cc2c3f69fb7ef82e98a3f6156e23cd2f6126cd
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:22:57 +0530

    Add Enhanced physics feature

---
+++ Assets/Tests/EditMode/BubblePhysicsTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests
+{
+    [TestFixture]
+    public class BubblePhysicsTest
+    {
+        [Test]
+        public void Test_Enhancedphysics_Works()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
