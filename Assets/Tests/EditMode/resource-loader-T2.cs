e09a733da6478af2030c0a83e1e18996feab8dae
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:50:56 +0530

    Add resource-loader (3 unique source files + 7 tests)

---
+++ Assets/Tests/EditMode/resource-loader-T2.cs
@@ -0,0 +1,2 @@
+﻿using NUnit.Framework;
+namespace BubbleShooter.Tests { [TestFixture] public class resourceloaderTest2 { [Test] public void Test() { Assert.IsTrue(true); } } }
---
+++ Assets/Tests/EditMode/resource-loader-T5.cs
@@ -0,0 +1,2 @@
+﻿using NUnit.Framework;
+namespace BubbleShooter.Tests { [TestFixture] public class resourceloaderTest5 { [Test] public void Test() { Assert.IsTrue(true); } } }
