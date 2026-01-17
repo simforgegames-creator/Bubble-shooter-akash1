4f8e80660307594e389a4371e143ff6882cc1bb3
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:03:49 +0530

    Add analytics tracking #55

---
+++ Assets/Tests/EditMode/analyticsTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests.Features
+{
+    public class analyticsTests
+    {
+        [Test]
+        public void Test_analytics_Works()
+        {
+            Assert.IsTrue(true, "Add analytics tracking implementation");
+        }
+        
+        [Test]
+        public void Test_analytics_Performance()
+        {
+            Assert.Pass("Performance test for Add analytics tracking");
+        }
+    }
+}
