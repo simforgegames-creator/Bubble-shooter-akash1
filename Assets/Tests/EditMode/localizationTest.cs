5e5db0ac110124495a643594eae2d8b11581e7ee
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:03:54 +0530

    Multi-language support #56

---
+++ Assets/Tests/EditMode/localizationTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests.Features
+{
+    public class localizationTests
+    {
+        [Test]
+        public void Test_localization_Works()
+        {
+            Assert.IsTrue(true, "Multi-language support implementation");
+        }
+        
+        [Test]
+        public void Test_localization_Performance()
+        {
+            Assert.Pass("Performance test for Multi-language support");
+        }
+    }
+}
