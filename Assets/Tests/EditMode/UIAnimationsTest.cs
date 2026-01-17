f0b603aa4dc682c535897ead0bc1a43f7698fd18
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:23:02 +0530

    Add UI transitions feature

---
+++ Assets/Tests/EditMode/UIAnimationsTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests
+{
+    [TestFixture]
+    public class UIAnimationsTest
+    {
+        [Test]
+        public void Test_UItransitions_Works()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
