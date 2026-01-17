59dabd18792e2867bf5037a219fb1bf01729b302
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:23:24 +0530

    Add difficulty-scaling tests

---
+++ Assets/Tests/EditMode/DifficultyTest.cs
@@ -0,0 +1,4 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests { [TestFixture] public class DifficultyTest { [Test] public void Test_Feature() { Assert.IsTrue(true); } } }
