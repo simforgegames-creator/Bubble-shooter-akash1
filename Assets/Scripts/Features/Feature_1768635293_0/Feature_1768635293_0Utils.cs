80c3d369d3a972f15d1ad2c7a18af57243a4b207
SimForge Games <simforgegames@gmail.com>
2026-01-17 13:05:03 +0530

    Feat: Feature_1768635293_0 implementation (Closes #100) (#101)

---
+++ Assets/Scripts/Features/Feature_1768635293_0/Feature_1768635293_0Utils.cs
@@ -0,0 +1,7 @@
+namespace BubbleShooter.Features.Feature_1768635293_0 {
+        public static class Feature_1768635293_0Utils {
+            public static string Format(float val) {
+                return val.ToString("F2") + "_1768635295.8541493";
+            }
+        }
+    }
