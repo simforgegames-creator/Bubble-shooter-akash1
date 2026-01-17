b39fd13b254b2fd8330a98aecfd8f458d806e4e3
Manav Savani <simforgegames@gmail.com>
2026-01-17 11:22:59 +0530

    Add Updated sound effects feature

---
+++ Assets/Tests/EditMode/SoundEffectsTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace BubbleShooter.Tests
+{
+    [TestFixture]
+    public class SoundEffectsTest
+    {
+        [Test]
+        public void Test_Updatedsoundeffects_Works()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
